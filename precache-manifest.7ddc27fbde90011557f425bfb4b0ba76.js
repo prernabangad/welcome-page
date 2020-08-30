d5ceb252197f604464f3ee61f61fd0a61e790e22
prernabangad <email address>
2020-08-30 21:34:42 +0530

    Updates

---
+++ precache-manifest.7ddc27fbde90011557f425bfb4b0ba76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108ce403a1e3710f46fbb6a237a468ec",
+    "url": "/welcome-page/index.html"
+  },
+  {
+    "revision": "9acb9866be1bd229589b",
+    "url": "/welcome-page/static/css/main.7644405d.chunk.css"
+  },
+  {
+    "revision": "b661ceef916524f889e0",
+    "url": "/welcome-page/static/js/2.9bdfea76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/welcome-page/static/js/2.9bdfea76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9acb9866be1bd229589b",
+    "url": "/welcome-page/static/js/main.cc0ad73c.chunk.js"
+  },
+  {
+    "revision": "86b1e31505d20d73c54c",
+    "url": "/welcome-page/static/js/runtime-main.5618fc08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/welcome-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
